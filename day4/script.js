45d65c4fc0f69461f5ce05944671657ab25ca6f7
Filipe Alves <filipealvesrds2019@gmail.com>
2023-03-25 21:41:02 -0300

    atualizar

---
+++ day4/script.js
@@ -0,0 +1,52 @@
+var meds = [];
+
+
+function addMed(){
+    event.preventDefault();
+
+    let newMed = {
+        "medicamento": document.getElementById('med').value,
+        "quantidade": document.getElementsByTagName('quant').value,
+        "tipo": document.getElementById('tipomed'),
+    }
+    resetForm();
+    meds.push(newMed);
+    renderTable();
+
+}
+
+function removeMed(index){
+    meds.splice(index, 1);
+    renderTable();
+}
+
+function renderTable(){
+    let table = document.getElementById('tabela');
+    table.innerHTML = `<th>ID</th>
+                    <th>Medicamento</th>
+                    <th>Quantidade</th>
+                    <th>Classe de Medicamento</th>
+                    <th>Remover</th>`;
+
+    for (let i = 0; i < meds.length; i++){
+
+
+        table.innerHTML = table.innerHTML  +  `<tr>
+
+
+        <td>${i}</td>
+        <td>${meds[i].medicamento}/td>
+        <td>${meds[i].quantidade}</td>
+        <td>${meds[i].tipo}</td>
+        <td><img src="trash-can-solid.svg" width="25px" height="25px" onclick="removeMed(${i})"></td>
+        </tr>
+        `
+    }
+}
+
+function resetForm(){
+    document.getElementById('med')
+    document.getElementById('quant')
+    document.getElementById('tipomed')
+}
+renderTable();
